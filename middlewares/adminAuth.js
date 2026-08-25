62c996c75043326ca7dc65c7e173857732e7b0e5
GiovLen <lenoxluc.gl@gmail.com>
2026-08-25 17:11:39 +0200

    Login

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.admin = decoded; // { username: 'admin' }
+    req.admin = decoded; 
     next();
   } catch (error) {
     return res.status(403).json({ error: 'Token non valido o scaduto' });
